c1ca6b0ec60d7dd82653b59fe0bf99a0af872c48
Arthur <arthurdbarnabas@gmail.com>
2025-05-27 16:56:55 +0800

    Debug Laravel routing and temporarily serve index.html directly

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{any}', function () {
-    return file_get_contents(public_path('index.html'));
+    Log::info('Catch-all route hit, serving index.html');
+    $path = public_path('index.html');
+    if (!file_exists($path)) {
+        Log::error("index.html not found at: {$path}");
+        return response('index.html not found', 404);
+    }
+    return file_get_contents($path);
 })->where('any', '.*');
