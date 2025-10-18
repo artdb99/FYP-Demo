5698d31d8980a9d18b430895f57a35a95b43d921
Arthur <arthurdbarnabas@gmail.com>
2025-10-18 17:44:39 +0800

    Once again 10

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -105,7 +105,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        os.getenv("FRONTEND_ORIGIN", "http://localhost:5174"),
+        os.getenv("FRONTEND_ORIGIN", "https://104384876laravel-cwh4axg4d4h5f0ha.southeastasia-01.azurewebsites.net"),
         os.getenv("LARAVEL_ORIGIN", "https://104384876laravel-cwh4axg4d4h5f0ha.southeastasia-01.azurewebsites.net"),
     ],
     allow_credentials=True,
